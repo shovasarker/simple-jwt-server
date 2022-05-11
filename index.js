08d3b67cc102568a0834f34f029d34340806e890
ShovaSarker <shovasarker01@gmail.com>
2022-05-11 12:12:54 +0600

    Createing jwt and send it to the client side on successful login

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -9,6 +11,24 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Simple JWT server is Running')
+})
+
+app.post('/login', (req, res) => {
+  const user = req.body
+  console.log(user)
+  //DANGER: Do not check password here for serious application
+  // USE proper process for hashing and checking
+  // AFTER completing all authentication related verfication , issue JWT token
+  if (user.email === 'user@gmail.com' && user.password === '123456') {
+    const accessToken = jwt.sign(
+      { email: user.email },
+      process.env.ACCESS_TOKE_SECRET,
+      { expiresIn: '1d' }
+    )
+    res.send({ success: true, accessToken: accessToken })
+  } else {
+    res.send({ success: false })
+  }
 })
 
 app.listen(port, () =>
